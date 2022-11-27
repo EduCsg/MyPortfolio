240b76eed049fffa49c321bae92e3d48c2f05c09
EduCsg <eduardocasagrande71@gmail.com>
2022-11-27 00:45:57 -0300

    Cria animação alternada nas skills

--- js/scroll-animation.js
+++ js/scroll-animation.js
@@ -1,16 +1,37 @@
-//skills hidden/show
+//skills show
 
 const observer = new IntersectionObserver((entries) => {
 	entries.forEach((entry) => {
 		if (entry.isIntersecting) {
 			entry.target.classList.add("showSkills");
 			entry.target.classList.remove("hiddenSkills");
-		} else {
-			entry.target.classList.remove("showSkills");
-			entry.target.classList.add("hiddenSkills");
 		}
+
+		// else {
+		// 	entry.target.classList.remove("showSkills");
+		// 	entry.target.classList.add("hiddenSkills");
+		// }
 	});
 });
 
 const hiddenElements = document.querySelectorAll(".hiddenSkills");
 hiddenElements.forEach((el) => observer.observe(el));
+
+//skil title show
+
+const observerSkill = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add("showSkillTitle");
+			entry.target.classList.remove("hiddenSkillTitle");
+		}
+
+		// else {
+		// 	entry.target.classList.remove("showSkills");
+		// 	entry.target.classList.add("hiddenSkills");
+		// }
+	});
+});
+
+const skillTitle = document.querySelectorAll(".skills-title");
+skillTitle.forEach((el) => observerSkill.observe(el));
